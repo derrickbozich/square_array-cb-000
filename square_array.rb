ef8c018db942033e35e88ae203fa5e1e5c8cce8e
derrick b <dbozich1@gmail.com>
2018-05-24 23:17:44 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,13 @@
 def square_array(array)
   # your code here
-  new_array =  array.each {|i|  i *= i}
+  new_array = []
+  array.each do |i|
+     new_array.push(i *= i)
+  end
   new_array
 end
+
+[1,2,3]
+
+
+square_array([1,2,3])
